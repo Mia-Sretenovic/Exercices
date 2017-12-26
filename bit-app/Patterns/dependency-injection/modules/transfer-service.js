17916b4137f622e03ddf27c2cb1c026a124c1e60
MiaS4 <sretenovic.mia@gmail.com>
2017-12-26 13:44:06 +0100

    Rearranging Exercices repository

---
+++ bit-app/Patterns/dependency-injection/modules/transfer-service.js
@@ -0,0 +1,19 @@
+class TrainTransferService {
+    transportTo(place) {
+        console.log(`Going to ${place} by train!`);
+    }
+}
+
+class PlaneTransferService {
+    transportTo(place) {
+        console.log(`Flying to ${place}!`);
+    }
+}
+
+
+//Dependency injection
+
+module.exports.Service = TrainTransferService;
+// module.exports.Service = PlaneTransferService;
+
+//By changing the dependency here, we change the whole app.
